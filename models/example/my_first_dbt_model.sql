9d2af8f8ffd63cfd1f43247cd7f513c070deef96
sumitgupta07.com@gmail.com <sumitgupta07.com@gmail.com>
2025-07-21 12:46:22 +0000

    Done

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,15 +1,9 @@
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
+{{ config(
+    tags=["priorty_main"] ) }}
 
 {{ config(
-    tags=["layerA"],
-    materialized='table') }}
+    materialized="table") }}
 
 with source_data as (
 
--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,9 +1,5 @@
-
--- Use the `ref` function to select from other models
-
 {{ config(
-    tags=["layerB"]
-) }}
+    tags=["priorty_main"] ) }}
 
 select *
 from {{ ref('my_first_dbt_model') }}
